1ba7b8469150a84e59e8fdced9f3f4785de7fc2d
Đình An <122213831+trndnhan@users.noreply.github.com>
2023-05-09 22:58:54 +0700

    Fix bug in MineButton.java

--- view/MineButton.java
+++ view/MineButton.java
@@ -1,106 +1,68 @@
 package view;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
+import javax.swing.JButton;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import javax.swing.BorderFactory;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.Timer;
 public class MineButton extends JButton {
-
-    private int number;
-
+    private int number = -1;
     private PanelPlayer p;
 
     public MineButton(PanelPlayer p) {
-        number = -1;
         this.p = p;
-        setPreferredSize(new Dimension(30, 30));
+        this.setPreferredSize(new Dimension(30, 30));
     }
 
-
-
-    @Override
     public void paint(Graphics g) {
-        switch (number) {
-
+        switch(this.number) {
             case -1:
-                g.drawImage(p.getGame().getGameFrame().getLoadData().getListImage().get("noUse"), 0, 0,
-                        getPreferredSize().width, getPreferredSize().height, null);
+                g.drawImage((Image)this.p.getGame().getGFrame().getLoadData().getListImage().get("noUse"), 0, 0, this.getPreferredSize().width, this.getPreferredSize().height, (ImageObserver)null);
                 break;
-
             case 0:
-                g.drawImage(p.getGame().getGameFrame().getLoadData().getListImage().get("b0"), 0, 0,
-                        getPreferredSize().width, getPreferredSize().height, null);
+                g.drawImage((Image)this.p.getGame().getGFrame().getLoadData().getListImage().get("b0"), 0, 0, this.getPreferredSize().width, this.getPreferredSize().height, (ImageObserver)null);
                 break;
-
             case 1:
-                g.drawImage(p.getGame().getGameFrame().getLoadData().getListImage().get("b1"), 0, 0,
-                        getPreferredSize().width, getPreferredSize().height, null);
+                g.drawImage((Image)this.p.getGame().getGFrame().getLoadData().getListImage().get("b1"), 0, 0, this.getPreferredSize().width, this.getPreferredSize().height, (ImageObserver)null);
                 break;
-
             case 2:
-                g.drawImage(p.getGame().getGameFrame().getLoadData().getListImage().get("b2"), 0, 0,
-                        getPreferredSize().width, getPreferredSize().height, null);
+                g.drawImage((Image)this.p.getGame().getGFrame().getLoadData().getListImage().get("b2"), 0, 0, this.getPreferredSize().width, this.getPreferredSize().height, (ImageObserver)null);
                 break;
-
             case 3:
-                g.drawImage(p.getGame().getGameFrame().getLoadData().getListImage().get("b3"), 0, 0,
-                        getPreferredSize().width, getPreferredSize().height, null);
+                g.drawImage((Image)this.p.getGame().getGFrame().getLoadData().getListImage().get("b3"), 0, 0, this.getPreferredSize().width, this.getPreferredSize().height, (ImageObserver)null);
                 break;
-
             case 4:
-                g.drawImage(p.getGame().getGameFrame().getLoadData().getListImage().get("b4"), 0, 0,
-                        getPreferredSize().width, getPreferredSize().height, null);
+                g.drawImage((Image)this.p.getGame().getGFrame().getLoadData().getListImage().get("b4"), 0, 0, this.getPreferredSize().width, this.getPreferredSize().height, (ImageObserver)null);
                 break;
-
             case 5:
-                g.drawImage(p.getGame().getGameFrame().getLoadData().getListImage().get("b5"), 0, 0,
-                        getPreferredSize().width, getPreferredSize().height, null);
+                g.drawImage((Image)this.p.getGame().getGFrame().getLoadData().getListImage().get("b5"), 0, 0, this.getPreferredSize().width, this.getPreferredSize().height, (ImageObserver)null);
                 break;
-
             case 6:
-                g.drawImage(p.getGame().getGameFrame().getLoadData().getListImage().get("b6"), 0, 0,
-                        getPreferredSize().width, getPreferredSize().height, null);
+                g.drawImage((Image)this.p.getGame().getGFrame().getLoadData().getListImage().get("b6"), 0, 0, this.getPreferredSize().width, this.getPreferredSize().height, (ImageObserver)null);
                 break;
-
             case 7:
-                g.drawImage(p.getGame().getGameFrame().getLoadData().getListImage().get("b7"), 0, 0,
-                        getPreferredSize().width, getPreferredSize().height, null);
+                g.drawImage((Image)this.p.getGame().getGFrame().getLoadData().getListImage().get("b7"), 0, 0, this.getPreferredSize().width, this.getPreferredSize().height, (ImageObserver)null);
                 break;
-
             case 8:
-                g.drawImage(p.getGame().getGameFrame().getLoadData().getListImage().get("b8"), 0, 0,
-                        getPreferredSize().width, getPreferredSize().height, null);
+                g.drawImage((Image)this.p.getGame().getGFrame().getLoadData().getListImage().get("b8"), 0, 0, this.getPreferredSize().width, this.getPreferredSize().height, (ImageObserver)null);
                 break;
             case 9:
-                g.drawImage(p.getGame().getGameFrame().getLoadData().getListImage().get("flag"), 0, 0,
-                        getPreferredSize().width, getPreferredSize().height, null);
+                g.drawImage((Image)this.p.getGame().getGFrame().getLoadData().getListImage().get("flag"), 0, 0, this.getPreferredSize().width, this.getPreferredSize().height, (ImageObserver)null);
                 break;
             case 10:
-                g.drawImage(p.getGame().getGameFrame().getLoadData().getListImage().get("bomb"), 0, 0,
-                        getPreferredSize().width, getPreferredSize().height, null);
+                g.drawImage((Image)this.p.getGame().getGFrame().getLoadData().getListImage().get("bomb"), 0, 0, this.getPreferredSize().width, this.getPreferredSize().height, (ImageObserver)null);
                 break;
             case 11:
-                g.drawImage(p.getGame().getGameFrame().getLoadData().getListImage().get("bombRed"), 0, 0,
-                        getPreferredSize().width, getPreferredSize().height, null);
+                g.drawImage((Image)this.p.getGame().getGFrame().getLoadData().getListImage().get("bombRed"), 0, 0, this.getPreferredSize().width, this.getPreferredSize().height, (ImageObserver)null);
                 break;
             case 12:
-                g.drawImage(p.getGame().getGameFrame().getLoadData().getListImage().get("bombX"), 0, 0,
-                        getPreferredSize().width, getPreferredSize().height, null);
-                break;
-            default:
-                break;
+                g.drawImage((Image)this.p.getGame().getGFrame().getLoadData().getListImage().get("bombX"), 0, 0, this.getPreferredSize().width, this.getPreferredSize().height, (ImageObserver)null);
         }
+
     }
 
     public void setNumber(int number) {
         this.number = number;
     }
-
 }
